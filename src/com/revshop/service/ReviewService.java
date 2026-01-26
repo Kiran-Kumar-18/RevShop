aa0f1f1e39416b8bb1fbf36b2ee0dd55a5bf0dbf
LingisettySwetha <lingisettyswethaa@gmail.com>
2026-01-26 23:02:41 +0530

    feature-payment complete payment flow with review integration

--- src/com/revshop/service/ReviewService.java
+++ src/com/revshop/service/ReviewService.java
@@ -1,20 +1,20 @@
 package com.revshop.service;
 
-import com.revshop.dao.IReviewDAO;
+import com.revshop.dao.IReviewDAOImpl;
+
 import com.revshop.dao.ReviewDAO;
+
 import com.revshop.model.Review;
 
-public class ReviewService implements IReviewService {
+public class ReviewService implements IReviewServiceImpl {
 
-    private IReviewDAO reviewDAO = new ReviewDAO();
+    private IReviewDAOImpl reviewDao = new ReviewDAO();
 
-    @Override
-    public boolean submitReview(Review review) {
-        return reviewDAO.addReview(review);
+    public ReviewService() throws Exception {
     }
 
     @Override
-    public boolean addReview(Review review) {
-        return false;
+    public boolean submitReview(Review review) {
+        return reviewDao.addReview(review);
     }
 }
